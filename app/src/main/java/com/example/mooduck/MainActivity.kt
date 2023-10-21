920ad23ff3e47c6b409d00f3a55b0417522088f1
dfoxs353 <platoleg37@gmail.com>
2023-10-21 21:28:33 +0300

    add EditText for password, button login, button sign_up and add logo

--- app/src/main/java/com/example/mooduck/MainActivity.kt
+++ app/src/main/java/com/example/mooduck/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.mooduck
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mooduck.databinding.ActivityLogInBinding
+import com.example.mooduck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent    )
     }
 }
