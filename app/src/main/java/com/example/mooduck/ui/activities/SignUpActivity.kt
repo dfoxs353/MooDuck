d8461166698ccdd7a142cefa6c3d1f10215f63ec
dfoxs353 <platoleg37@gmail.com>
2023-10-28 10:48:39 +0300

    update sign up activity

--- app/src/main/java/com/example/mooduck/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/mooduck/ui/activities/SignUpActivity.kt
@@ -1,10 +1,18 @@
 package com.example.mooduck.ui.activities
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.mooduck.R
 import com.example.mooduck.databinding.ActivitySignUpBinding
 import com.example.mooduck.ui.fragments.ModalMailFragment
 import com.example.mooduck.ui.viewmodel.SignUpViewModel
@@ -23,40 +31,124 @@ class SignUpActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
 
-        binding.signupButton.setOnClickListener {
-            signUp()
-        }
+        val signup = binding.signupButton
+        val login = binding.loginButton
 
-        binding.loginButton.setOnClickListener {
+        val username = binding.nameInput
+        val maiil = binding.mailInput
+        val firstPassword = binding.passwordFirstInput
+        val secondPassword = binding.passwordSecondInput
+
+        val usernameLayout = binding.nameInputLayout
+        val maiilLayout = binding.mailInputLayout
+        val firstPasswordLayout = binding.passwordFirstInputLayout
+        val secondPasswordLayout = binding.passwordSecondInputLayout
+
+        login.setOnClickListener {
             finish()
         }
+
+        viewModel.signupFormState.observe( this@SignUpActivity, Observer {
+            val loginState = it ?: return@Observer
+
+            login.isEnabled = loginState.isDataValid
+
+            if (loginState.usernameError != null) {
+                usernameLayout.isErrorEnabled = true
+                usernameLayout.error = getString(loginState.usernameError)
+            }
+            else{
+                usernameLayout.isErrorEnabled =false
+            }
+
+            if (loginState.passwordError != null) {
+                firstPasswordLayout.error = getString(loginState.passwordError)
+
+                secondPasswordLayout.error = getString(loginState.passwordError)
+            }
+            else{
+                secondPasswordLayout.isErrorEnabled =false
+            }
+        })
+
+        viewModel.signupResult.observe(this@SignUpActivity, Observer {
+            val signupResult = it ?: return@Observer
+
+            if (signupResult.error != null) {
+                showLoginFailed(signupResult.error)
+            }
+            if (signupResult.success != null) {
+                updateUiWithUser(signupResult.success.user.username)
+            }
+            setResult(Activity.RESULT_OK)
+        })
+
+        username.afterTextChanged {
+            viewModel.loginDataChanged(
+                username.text.toString(),
+                firstPassword.text.toString()
+            )
+        }
+
+        firstPassword.apply {
+            afterTextChanged {
+                viewModel.loginDataChanged(
+                    username.text.toString(),
+                    firstPassword.text.toString()
+                )
+            }
+
+            setOnEditorActionListener { _, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_DONE ->
+                        signup(
+                            username.text.toString(),
+                            maiil.text.toString(),
+                            firstPassword.text.toString()
+                        )
+                }
+                false
+            }
+
+            signup.setOnClickListener {
+                signup(username.text.toString(),maiil.text.toString(), firstPassword.text.toString())
+            }
+        }
+
     }
 
-    private fun signUp() {
-//        val mail = binding.mailInput.text.toString()
-//        val password = binding.passwordFirstInput.text.toString()
-//        val username = binding.nameInput.text.toString()
-//
-//        GlobalScope.launch(Dispatchers.Main) {
-//            val fragmentMail = ModalMailFragment()
-//            fragmentMail.show(supportFragmentManager, "modalMailFragment")
-//
-//            val response = viewModel.signup(mail,password, username)
-//            if(response != null){
-//                Log.d("TAG","log")
-//                showToast("Успешная регистрация ${response.user.username}")
-//            }
-//            else{
-//                Log.d("TAG", "no log")
-//                showToast("Ошибка")
-//            }
-//        }
+
+
+    private fun signup(username: String, mail:String,password:String) {
+        GlobalScope.launch(Dispatchers.Main) {
+            viewModel.signup(username,mail,password)
+        }
     }
 
-    private fun showToast(text: String){
-        val duration = Toast.LENGTH_SHORT
-        val toastLogin = Toast.makeText(this, text,duration)
-
-        toastLogin.show()
+    private fun updateUiWithUser(name: String) {
+        val welcome = getString(R.string.welcome)
+        // TODO : initiate successful logged in experience
+        Toast.makeText(
+            applicationContext,
+            "$welcome $name",
+            Toast.LENGTH_LONG
+        ).show()
     }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
 }
--- app/src/main/java/com/example/mooduck/ui/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/example/mooduck/ui/viewmodel/SignUpViewModel.kt
@@ -1,28 +1,65 @@
 package com.example.mooduck.ui.viewmodel
 
 import android.util.Log
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mooduck.R
 import com.example.mooduck.common.RetrofitClient
 import com.example.mooduck.data.remote.auth.AuthApi
 import com.example.mooduck.data.remote.auth.AuthResponse
+import com.example.mooduck.data.remote.auth.AuthResult
+import com.example.mooduck.data.remote.auth.Result
 import com.example.mooduck.data.repository.UserRepository
+import com.example.mooduck.ui.model.AuthFormState
 import kotlinx.coroutines.Dispatchers
 
 class SignUpViewModel: ViewModel() {
+
+    private val _signupForm = MutableLiveData<AuthFormState>()
+    val signupFormState: LiveData<AuthFormState> = _signupForm
+
+    private val _signupResult = MutableLiveData<AuthResult>()
+    val signupResult: LiveData<AuthResult> = _signupResult
 
     private val retrofit = RetrofitClient.instance
     private val userApi = retrofit.create(AuthApi::class.java)
 
     private val userRepository: UserRepository = UserRepository(userApi, Dispatchers.IO)
 
-//    suspend fun signUp(email: String, password: String, username: String): AuthResponse? {
-//        val response = userRepository.signup(email, password, username)
-//
-//        if(response !=null){
-//            Log.d("TAG", "user ${response.user.username}")
-//            return response
-//        }
-//
-//        return null
-//    }
+    suspend fun signup(username: String,email: String, password: String) {
+        val result = userRepository.signup(email, password,username)
+
+        if(result is Result.Success){
+            _signupResult.value = AuthResult(success = result.data)
+        }
+        else{
+            _signupResult.value = AuthResult(error = R.string.error_string)
+        }
+    }
+
+    fun loginDataChanged(username: String, password: String) {
+        if (!isUserNameValid(username)) {
+            _signupForm.value = AuthFormState(usernameError = R.string.invalid_username)
+        } else if (!isPasswordValid(password)) {
+            _signupForm.value = AuthFormState(passwordError = R.string.invalid_password)
+        } else {
+            _signupForm.value = AuthFormState(isDataValid = true)
+        }
+    }
+
+    private fun isUserNameValid(username: String): Boolean {
+        return if(username.contains('@')){
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        } else{
+            username.isNotBlank()
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean{
+        return password.length > 5
+    }
+
+
 }
